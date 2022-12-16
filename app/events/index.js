b42363f4a83594773697c0e46d82fb29996aa061
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-16 19:50:14 +0200

    Update

--- app/events/index.js
+++ app/events/index.js
@@ -8,23 +8,24 @@ import * as telegrafHelper from '../helpers/telegraf.js';
 export default (bot) => bot
   .use(errorHandler)
 
-// Standard events (additional information)
-// .command('start')
-// .command('help')
-// .command('about')
-// .command('settings')
-
-// Banners pulling events
+  // Banners pulling events
   .command('wish', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersWish))
   .command('wish10', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersWishX10))
 
-// Users information
+  .action(/^wi /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersWish))
+  .action(/^wi10 /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersWishX10))
+
+  // Users information
   .command('inventory', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersInventory))
   .command('profile', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersProfile))
   .command('history', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersHistory))
-// .command('referral')
 
-// Shop events
+  .action(/^hi /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersHistory))
+  .action(/^pr /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersProfile))
+  .action(/^pr get /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersPrimogems))
+  .action(/^in /, telegrafHelper.getDataByChatId(gachaSimulatorRest.usersInventory))
+
+  // Shop events
   .command('primogems', telegrafHelper.getDataByChatId(gachaSimulatorRest.usersPrimogems))
 
   .use(replyModule)
