e68caceb2606cadd2ab1f409f6d033de3b0fd9a7
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-17 18:29:17 +0200

    Update

--- app/helpers/telegrafReplyHelper.js
+++ app/helpers/telegrafReplyHelper.js
@@ -56,6 +56,7 @@ export function message({
     {
       text: messageTemplate,
       parse_mode: 'HTML',
+      disable_web_page_preview: true,
     },
     { ...makeMarkupTelegrafButtons(media?.mediaMarkupButtons) },
   );
