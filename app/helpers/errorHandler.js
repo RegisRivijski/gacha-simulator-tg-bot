5f2106e133e2dcc2507248d98059f2354d0c4859
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-30 21:18:39 +0200

    Update

--- app/helpers/errorHandler.js
+++ app/helpers/errorHandler.js
@@ -1,9 +1,3 @@
 export default function errorHandler(e) {
-  console.error('[ERROR] =========================== BEGIN ===========================');
-  console.error('Message:', e.message);
-  console.error('Code:', e.code);
-  console.error('Stack:', e.stack);
-  if (e.request) console.error('Request:', e.request.method, e.request.path);
-  if (e.response) console.error('Response:', e.response.status, e.response.data);
-  console.error('[ERROR] ============================ END ============================');
+  console.error(e);
 }
