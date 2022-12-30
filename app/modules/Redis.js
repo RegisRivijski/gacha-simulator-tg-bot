7649d00174717376d37d4442bc557eba5366b2a2
Artur Petrov <artur.petrov@auto.ria.com>
2022-12-30 19:43:00 +0200

    Update

--- app/modules/Redis.js
+++ app/modules/Redis.js
@@ -6,8 +6,4 @@ client.on('open', () => {
   console.info('Successfully connected to Redis.');
 });
 
-client.on('error', (e) => {
-  throw new Error(`Redis Client Error: ${e.message}.`);
-});
-
 export default client;
